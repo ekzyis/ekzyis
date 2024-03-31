2479e9aeab36caab2dcd712381c4a57b7bd107e2
ekzyis <ekzyis@ekzy.is>
2024-03-31 17:09:57 +0200

    Also deploy .well-known/

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 set -e
 
 ENV=production make -B
-rsync -avh public/ ekzyis.com:/var/www/ekzyis --delete --dry-run
+rsync -avh .well-known public/ ekzyis.com:/var/www/ekzyis --delete --dry-run
 
 echo
 read -p "Continue deploy? [yn] " yn
 echo
-[ "$yn" == "y" ] && rsync -avh public/ ekzyis.com:/var/www/ekzyis --delete
+[ "$yn" == "y" ] && rsync -avh .well-known public/ ekzyis.com:/var/www/ekzyis --delete
